5bb65b2a0b111666da7d3a7fe493322af3042624
strzelczykn <56188140+strzelczykn@users.noreply.github.com>
2019-11-10 11:21:55 +0100

    Add files via upload

---
+++ tablice tablic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tablica_tablic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] tab = new int[][]
+            {
+                new int [] {1,2},
+                new int []{3,4},
+                new int [] {6},
+
+
+            };
+
+            /* Wyświetl zawartośc tablicy za pomocą dwóch pętli for*/
+
+            for (int i = 0; i < tab.GetLength(0); i++) 
+            {
+                for (int j = 0; j < tab[i].Length; j++) 
+                {
+                    Console.Write("{0}", tab[i][j]);
+
+                }
+                Console.WriteLine();
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
